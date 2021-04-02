900de778025bbc48aad2997db3778fb6868a92b1
Patryk Gronkiewicz <patryk@gronkiewicz.xyz>
2021-04-03 00:19:01 +0200

    add switching themes

---
+++ .local/bin/day.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm $HOME/.cache/vim_night
+ln -sf $HOME/.config/kitty/light.conf $HOME/.config/kitty/theme.conf
---
+++ .local/bin/night.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+touch $HOME/.cache/vim_night
+ln -sf $HOME/.config/kitty/dark.conf $HOME/.config/kitty/theme.conf
