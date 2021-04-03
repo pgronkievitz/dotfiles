db014188400382a4eaae1258654fb492e4b735ae
Patryk Gronkiewicz <patryk@gronkiewicz.xyz>
2021-04-03 09:43:11 +0200

    add changing themes in bat

--- .local/bin/day.sh
+++ .local/bin/day.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm $HOME/.cache/vim_night
 ln -sf $HOME/.config/kitty/light.conf $HOME/.config/kitty/theme.conf
+ln -sf $HOME/.config/bat/config_light $HOME/.config/bat/config
--- .local/bin/night.sh
+++ .local/bin/night.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 touch $HOME/.cache/vim_night
 ln -sf $HOME/.config/kitty/dark.conf $HOME/.config/kitty/theme.conf
+ln -sf $HOME/.config/bat/config_dark $HOME/.config/bat/config
