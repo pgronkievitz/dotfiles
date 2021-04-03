73d1e4d32fd8911b0676a2286b9961e6e28c2e2c
Patryk Gronkiewicz <patryk@gronkiewicz.xyz>
2021-04-03 20:02:56 +0200

    add rofi to day/night switcheroo

--- .local/bin/day.sh
+++ .local/bin/day.sh
@@ -2,3 +2,4 @@
 rm $HOME/.cache/vim_night
 ln -sf $HOME/.config/kitty/light.conf $HOME/.config/kitty/theme.conf
 ln -sf $HOME/.config/bat/config_light $HOME/.config/bat/config
+ln -sf $HOME/.config/rofi/config_light $HOME/.config/rofi/config
--- .local/bin/night.sh
+++ .local/bin/night.sh
@@ -2,3 +2,4 @@
 touch $HOME/.cache/vim_night
 ln -sf $HOME/.config/kitty/dark.conf $HOME/.config/kitty/theme.conf
 ln -sf $HOME/.config/bat/config_dark $HOME/.config/bat/config
+ln -sf $HOME/.config/rofi/config_dark $HOME/.config/rofi/config
